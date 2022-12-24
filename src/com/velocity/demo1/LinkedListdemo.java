9ed0b2d01361b65804c91bcba0da9304b50074b2
suyog0410 <gawalisuyog04@gmail.com>
2022-12-24 14:51:19 +0530

    second commit : added code for linkedlist

---
+++ src/com/velocity/demo1/LinkedListdemo.java
@@ -0,0 +1,18 @@
+package com.velocity.demo1;
+
+import java.util.LinkedList;
+
+public class LinkedListdemo {
+
+	
+	 public static void main(String[] args) {
+		
+		 LinkedList<Integer> list=new LinkedList<Integer>();
+		 
+		 list.add(1);
+		 list.add(2);
+		 list.add(3);
+		 
+		 System.out.println(list);
+	}
+}
