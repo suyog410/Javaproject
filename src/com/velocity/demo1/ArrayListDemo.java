74d9ba88aee2537e4f48e20212a67d1d98ab5210
suyog0410 <gawalisuyog04@gmail.com>
2022-12-24 14:47:05 +0530

    first commit : added arraylist demo code

---
+++ src/com/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("bunny");
+		list.add("pranav");
+		list.add("shubham");
+		System.out.println(list);
+	}
+}
